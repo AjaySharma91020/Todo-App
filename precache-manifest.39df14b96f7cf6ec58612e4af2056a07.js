4124565f042b885314a15d08b523f11b5d83fd60
AjaySharma91020 <ajsharma7231994697@gmail.com>
2020-10-13 21:04:21 +0530

    Updates

---
+++ precache-manifest.39df14b96f7cf6ec58612e4af2056a07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "550f0ed60c0aba4cd681c0a94b6112f9",
+    "url": "/Todo-App/index.html"
+  },
+  {
+    "revision": "6aff6547c83410f04dca",
+    "url": "/Todo-App/static/css/main.11b1cbcd.chunk.css"
+  },
+  {
+    "revision": "3ffb932ad529857215e2",
+    "url": "/Todo-App/static/js/2.d26e75b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo-App/static/js/2.d26e75b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6aff6547c83410f04dca",
+    "url": "/Todo-App/static/js/main.a373e285.chunk.js"
+  },
+  {
+    "revision": "32ae59bf0b31915a0ee0",
+    "url": "/Todo-App/static/js/runtime-main.33efac75.js"
+  }
+]);
